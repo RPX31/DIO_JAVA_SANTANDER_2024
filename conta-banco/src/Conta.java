74734cd98ba837d7662d4d111efe5b4b3b37f902
Rodrigo Oliveira de Santana <pracimaestudos@gmail.com>
2025-07-07 20:13:15 -0300

    removida funcao transferir e finalização do projeto

--- conta-banco/src/Conta.java
+++ conta-banco/src/Conta.java
@@ -6,17 +6,24 @@ public class Conta {
     double saldo;
 
 
-    public Conta(){};
+    public Conta(){}
 
-    
-    public void buscarConta(int numeroDaConta){
-
-       numeroDaConta = this.getNumero();
-
-        System.out.println("Conta encontrada" + numeroDaConta);
+     public void visualizarConta() {
+        System.out.println("==== Dados da Conta ====");
+        System.out.println("Cliente: " + this.nomeCliente);
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Número da Conta: " + this.numero);
+        System.out.printf("Saldo: R$ %.2f%n", this.saldo);
+        System.out.println("========================");
+    }
+    public void buscarConta(int numeroDaConta) {
+        if (this.numero == numeroDaConta) {
+            System.out.println("Conta encontrada: " + this.numero);
+        } else {
+            System.out.println("Conta não encontrada.");
+        }
     }
     public void depositar(double valor){
-
         this.setSaldo(this.getSaldo() + valor ) ;
     }
     public int getNumero() {
@@ -37,10 +44,10 @@ public class Conta {
     public void setNomeCliente(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
-    public Double getSaldo() {
+    public double getSaldo() {
         return saldo;
     }
-    public void setSaldo(Double saldo) {
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 }
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,52 +1,74 @@
+
 import java.util.Scanner;
 
 public class ContaTerminal{
 
     
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        Conta conta = new Conta();
         
         while (true) {
                 System.out.println("Digitem entre as opções 0 Até 4");
                 System.out.println("1 - Criar conta");
                 System.out.println("2 - depositar");
-                System.out.println("3 - Transferir");
-                System.out.println("4 - extrato conta");
+                System.out.println("3 - extrato conta");
                 System.out.println("0 -  sair do sistema");
 
             
-            int opcao = entrada.nextInt();
+            int opcao = scanner.nextInt();
             switch(opcao){
                 case 1:
-                System.out.println("Por favor, digite o número da Agência !");
-                Conta conta1 = new Conta();
-                System.out.println("*(depois ENTER para o próximo campo)*");
-                
+                    System.out.println("Por favor, digite o número da Conta:");
+                    conta.setNumero(scanner.nextInt());
+                    scanner.nextLine(); 
+
+                    System.out.println("Por favor, digite o número da Agência:");
+                    conta.setAgencia(scanner.nextLine());
+
+                    System.out.println("Por favor, digite o nome do Cliente:");
+                    conta.setNomeCliente(scanner.nextLine());
+
+                    System.out.println("Por favor, digite o saldo inicial:");
+                    conta.setSaldo(scanner.nextDouble());
+
+                    System.out.println("Olá "+ conta.getNomeCliente() +", obrigado por criar uma conta em nosso banco, sua agência é "+ conta.getAgencia()+", conta "+conta.getNumero()+" e seu saldo "+conta.getSaldo()+" já está disponível para saque.");
+                            
                 break;
                 case 2:
+                System.out.println("Por favor, digite o valor do depósito");
+                double valorDeposito = scanner.nextDouble();
 
-                break;
+                System.out.println("Por favor, digite o número da Conta que deseja depositar:");
+                int numeroBuscado = scanner.nextInt();
+
+                   if (conta.getNumero() == numeroBuscado) {
+                        System.out.println("Conta encontrada!");
+                        conta.depositar(valorDeposito);
+                    } else {
+                        System.out.println("Conta não encontrada.");
+                    }
+
+                break;          
                 case 3:
+                System.out.println("Digite o número da conta:");
 
-                break;
-                case 4:
+                     numeroBuscado = scanner.nextInt();
 
+                    if (conta.getNumero() == numeroBuscado) {
+                        System.out.println("Conta encontrada!");
+                        conta.visualizarConta();
+                    } else {
+                        System.out.println("Conta não encontrada.");
+                    } 
                 break;
                 case 0:
                 System.out.println("Encerrando o sistema. Até mais!");
-                    entrada.close(); 
+                    scanner.close(); 
                     return; 
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
-             
-
-
         }
-            
-        
-
-    }
-    
+    }  
     }
     }
